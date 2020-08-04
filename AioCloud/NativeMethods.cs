573709e68b0fb4a51be5fbee552e95adce086a95
Connection Refused <cutecr@pm.me>
2020-08-05 06:50:26 +0800

    Add NativeMethods

---
+++ AioCloud/NativeMethods.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AioCloud
+{
+    public static class NativeMethods
+    {
+        public enum NameList : int
+        {
+            TYPE_TCPHost,
+            TYPE_TCPPort,
+            TYPE_TCPPass,
+            TYPE_TCPMeth,
+
+            TYPE_UDPHost,
+            TYPE_UDPPort,
+            TYPE_UDPPass,
+            TYPE_UDPMeth
+        }
+
+        [DllImport("Redirector.bin", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool aio_init();
+
+        [DllImport("Redirector.bin", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool aio_dial(
+            NameList name,
+            [MarshalAs(UnmanagedType.LPStr)] string value
+        );
+
+        [DllImport("Redirector.bin", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool aio_load();
+
+        [DllImport("Redirector.bin", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool aio_free();
+    }
+}
